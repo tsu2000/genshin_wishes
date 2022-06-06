c7b96bebbc3aed3f159178efd5b3e68425dd7ac3
tsu2000 <106811131+tsu2000@users.noreply.github.com>
2022-06-06 11:20:47 +0800

    Update genshin_5star_chances.py

--- genshin_5star_chances.py
+++ genshin_5star_chances.py
@@ -70,7 +70,7 @@ def pceb(): # Permanent/Character Event Banner
     
     st.markdown('What each graph means:')
     st.markdown('- **Probability Mass Function (PMF):** The base per-pull probability of obtaining a 5-Star Character on each individual pull after pity reset')
-    st.markdown('- **Cumulative Probability Distribution (CDF):** The cumulative probability of obtaining a 5-Star Character on your current pull or before that')
+    st.markdown('- **Cumulative Distribution Function (CDF):** The **cumulative** probability of obtaining a 5-Star Character on your current pull or before that')
     st.markdown('- **Distribution of Successful Pulls:** Where 5-Star Character drops are most likely to occur between the pity reset and hard pity')
     
     st.write("###")
@@ -144,7 +144,7 @@ def pceb(): # Permanent/Character Event Banner
     roll_cum_dict = {num: roll_cum_probs(num) for num in range(1, 91)}
     
     
-    st.markdown('### Cumulative Probability Distribution (CDF)')
+    st.markdown('### Cumulative Distribution Function (CDF)')
     xc = st.slider('Choose number of pulls after your last 5-Star Character to see the cumulative probabilities of getting a 5-star character within your set number of pulls:', 1, 90, 30)
     
     
@@ -248,7 +248,7 @@ def web(): # Weapon Event Banner
     
     st.markdown('What each graph means:')
     st.markdown('- **Probability Mass Function (PMF):** The base per-pull probability of obtaining a 5-Star Weapon on each individual pull after pity reset')
-    st.markdown('- **Cumulative Probability Distribution (CDF):** The cumulative probability of obtaining a 5-Star Weapon on your current pull or before that')
+    st.markdown('- **Cumulative Distribution Function (CDF):** The **cumulative probability** of obtaining a 5-Star Weapon on your current pull or before that')
     st.markdown('- **Distribution of Successful Pulls:** Where 5-Star Weapon drops are most likely to occur between the pity reset and hard pity')
     
     st.write("###")
@@ -322,7 +322,7 @@ def web(): # Weapon Event Banner
 
     roll_cum_dict = {num: roll_cum_probs(num) for num in range(1, 78)}
     
-    st.markdown('### Cumulative Probability Distribution (CDF)')
+    st.markdown('### Cumulative Distribution Function (CDF)')
     xc = st.slider('Choose number of pulls after your last 5-Star Weapon to see the cumulative probabilities of getting a 5-star Weapon within your set number of pulls:', 1, 77, 25)
     
     
