dd74c046dd88ac444dfddaf83ec4b511a45b5a03
tsu2000 <106811131+tsu2000@users.noreply.github.com>
2025-05-27 00:19:47 +0800

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -26,7 +26,7 @@ def main():
     st.markdown('---')
 
     st.markdown('#### Current Features:')
-    st.markdown('- 🎲 **Wish System Probabilities**: Data visualisations and statistical explanations behind the wish and pity system probability rates in Genshin Impact (for 5★ items/characters).')
+    st.markdown('- 🎲 **5★ Wish System Overview**: Data visualisations and statistical explanations behind the wish and pity system probability rates in Genshin Impact (for 5★ items/characters).')
     st.markdown('- 🕹️ **Drop Rate Simulator**: Customize your wish history to simulate expected outcomes based on pity count, banner type, and number of pulls.')
 
     st.markdown('---')
